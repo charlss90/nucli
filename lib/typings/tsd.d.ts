284e2e43ba4eee244cf856987bdf3f7686b81308
Carlos E. Pazmiño Peralta <carlos.pazmino.developer@gmail.com>
2015-10-27 23:15:27 +0100

    Refactoring

---
+++ lib/typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="node/node.d.ts" />
+/// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="sequelize/sequelize.d.ts" />
+/// <reference path="validator/validator.d.ts" />
+/// <reference path="async/async.d.ts" />
